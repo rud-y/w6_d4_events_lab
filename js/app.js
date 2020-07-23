b789adee725a32b400af43761c5f8843dfae0690
rudy <rudolf.zachar@gmail.com>
2020-07-23 16:45:33 +0100

    whatever we have so far

--- js/app.js
+++ js/app.js
@@ -1,3 +1,35 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('JavaScript loaded');
+
+  const form = document.querySelector("#new-item-form");
+  form.addEventListener('submit', handleFormSubmit);
 })
+
+const handleFormSubmit = function(event) {
+  event.preventDefault();
+  const resultParagraph = document.querySelector("#reading-list");
+        resultParagraph.appendChild(listItemMaster);
+        restultParagraph.textContent = this;
+   
+  const listItemMaster = document.createElement('li');
+      listItemMaster.textContent = list;
+      
+
+const listItemTitle = document.createElement('li');
+      listItemTitle.textContent = event.target.title.value;
+      listItemTitle.classList.add('title');
+const listItemAuthor = document.createElement('li');
+      listItemAuthor.textContent = event.target.author.value;
+      listItemAuthor.classList.add('author');
+const listItemCategory = document.createElement('li');
+      listItemCategory.textContent = event.target.category.value;
+      listItemCategory.classList.add('category');
+ 
+
+const list = document.createElement('ul');
+    list.appendChild(listItemTitle);
+    list.appendChild(listItemAuthor);
+    list.appendChild(listItemCategory);
+      
+};
+
